7f0c577ec279756c1c41b010152f3829848d3be2
Daniel Scrivano <scrivinator@gmail.com>
2013-04-23 05:18:45 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import render_template
+from hue import Hue
+import requests
+
+h = Hue();
+h.station_ip = "192.168.1.100"
+h.get_state();
+l = h.lights.get('l1')
+
+#l.on()
+#l.bri(255)
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello(name=None):
+	return render_template('jshtml.html')
+
+@app.route('/changecolor/<int:red>/<int:green>/<int:blue>')
+def changecolor(red,green,blue):
+	l.rgb(red, green, blue)
+	return 'pray'
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0',debug=True,port=80)
